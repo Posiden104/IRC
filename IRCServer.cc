b0258080ddd455f23e4f42696df8cf15070294cc
Joel Van Auken <jvanauke@purdue.edu>
2015-05-03 23:10:33 -0400

    Make IRCServer

--- IRCServer.cc
+++ IRCServer.cc
@@ -245,6 +245,8 @@ IRCServer::enterRoom(int fd, const char * username, const char * password, const
 		// See if the user is already in the room
 		if(!findUser(username, &usr, rm->users)) {	
 			findUser(username, &usr, &_users);
+			char *puname = strdup(usr->username);
+			char *ppass = strdup(usr->password);
 			User *u = (User*)calloc(1, sizeof(User));
 			u = usr;
 			//u->username = strdup(usr->username);
